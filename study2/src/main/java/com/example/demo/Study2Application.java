7f4de76f0b729602c72caaf7774aaf3d9cc3284c
itmybaby <476771756@qq.com>
2017-12-22 09:30:13 +0800

    springboot 退出

--- study2/src/main/java/com/example/demo/Study2Application.java
+++ study2/src/main/java/com/example/demo/Study2Application.java
@@ -3,8 +3,13 @@ package com.example.demo;
 
 import javafx.scene.Parent;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Study2Application {
@@ -21,4 +26,20 @@ public class Study2Application {
                 .run(args);
 
     }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
 }
