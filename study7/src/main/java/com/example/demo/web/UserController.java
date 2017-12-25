efc394d6fe0bd48f27e82a946afca7b64f28079d
itmybaby <476771756@qq.com>
2017-12-25 14:22:15 +0800

    springboot系列教程（七）——整合mybatis springboot系列教程（八）——整合thymeleaf、freemarker

--- study7/src/main/java/com/example/demo/web/UserController.java
+++ study7/src/main/java/com/example/demo/web/UserController.java
@@ -3,8 +3,10 @@ package com.example.demo.web;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -13,14 +15,33 @@ import java.util.List;
  * @date :2017/12/25
  */
 
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @RequestMapping("/")
+    @ResponseBody
     public List<User> list(){
         return userService.findAccountList();
+    }
+
+    /**
+     * 返回html模板.
+     */
+    @RequestMapping("/hello")
+    public  String hello(ModelMap map){
+        map.put("hello","hello world");
+        return "helloHtml";
+    }
+
+    /**
+     * 返回Ftl模板.
+     */
+    @RequestMapping("/helloFtl")
+    public String helloFtl(ModelMap map){
+        map.put("hello","from TemplateController.helloFtl");
+        return"/helloFtl";
     }
 }
