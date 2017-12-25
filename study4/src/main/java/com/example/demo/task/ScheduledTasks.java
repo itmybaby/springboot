1554e99e141ea7c2ba523d5d6f58b737a4f44db3
itmybaby <476771756@qq.com>
2017-12-25 13:29:59 +0800

    springboot系列文章（四）——fastjson、定时任务、全局异常配置

---
+++ study4/src/main/java/com/example/demo/task/ScheduledTasks.java
@@ -0,0 +1,25 @@
+package com.example.demo.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author :小虎
+ * @date :2017/12/25
+ */
+
+
+@Component
+public class ScheduledTasks {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+
+}
