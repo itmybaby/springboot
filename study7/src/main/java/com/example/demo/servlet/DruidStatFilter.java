3b07e99405d15d449e2db0a165674aec80631b87
itmybaby <476771756@qq.com>
2017-12-26 11:04:56 +0800

    springboot系列教程（七）——整合mybatis springboot系列教程（八）——整合thymeleaf、freemarker druid

---
+++ study7/src/main/java/com/example/demo/servlet/DruidStatFilter.java
@@ -0,0 +1,22 @@
+package com.example.demo.servlet;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * druid过滤器.
+ *
+ * @author :小虎
+ * @date :2017/12/26
+ */
+
+@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*",
+        initParams = {
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+        }
+)
+public class DruidStatFilter extends WebStatFilter {
+
+}
